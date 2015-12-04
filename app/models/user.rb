d6bf16cc244391145b776244f91338999d437fd6
Josh <jalexy12@gmail.com>
2015-12-04 11:11:19 -0500

    Added enum to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:admin, :author, :regular_user]
+
+  before_create :set_default_role, if: :new_record?
+
+  def set_default_role
+  	self.role ||= :regular_user
+  end
 end
